75a0036417da70c9cfc5f903f805e047eb3f3e6e
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-01 12:08:05 +0200

    feat: add loading spinner

---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import "./Loading.css";
+
+export default function Loading() {
+  return (
+    <main className="bg-[#d5d6d9] h-screen flex items-center justify-center">
+      <div className="lds-ring">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </main>
+  );
+}
