4e1f4ed1036c4b087e858546f89d30a7418d84d0
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-01 12:06:17 +0200

    fix: create type for air_pollution and forecast item

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,125 @@
-export type Weather = {
-  city: string;
-  wind_speed: number;
-  pressure: number;
+export type CurrentWeather = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: Array<{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
   visibility: number;
-  air_quality: number;
-  temp: number;
-  humidity: number;
-  description: string;
-  sunrise: number;
-  sunset: number;
-  rain: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  rain?: {
+    "1h": number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
+export type ForecastWeather = {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: Array<ForecastItem>;
+  city: {
+    id: number;
+    name: string;
+    coord: {
+      lat: number;
+      lon: number;
+    };
+    country: string;
+    population: number;
+    timezone: number;
+    sunrise: number;
+    sunset: number;
+  };
 };
 
 export type ForecastItem = {
-  description: string;
-  temp_min: number;
-  temp_max: number;
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: Array<{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  rain?: {
+    "3h": number;
+  };
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
 };
 
-export type Forecast = ForecastItem[];
+export type AirPollution = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  list: Array<{
+    main: {
+      aqi: number;
+    };
+    components: {
+      co: number;
+      no: number;
+      no2: number;
+      o3: number;
+      so2: number;
+      pm2_5: number;
+      pm10: number;
+      nh3: number;
+    };
+    dt: number;
+  }>;
+};
