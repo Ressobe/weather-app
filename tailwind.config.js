263146e5244fdc762fced1c8a865d9d914d0f2c8
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-01 12:59:06 +0200

    feat: add colors using css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        on_primary: "rgb(var(--color-on-primary) / <alpha-value>)",
+        background: "rgb(var(--color-background) / <alpha-value>)",
+        on_background: "rgb(var(--color-on-background) / <alpha-value>)",
+        surface: "rgb(var(--color-surface) / <alpha-value>)",
+        on_surface: "rgb(var(--color-on-surface) / <alpha-value>)",
+        outline: "rgb(var(--color-outline) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
