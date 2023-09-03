2b6ce0a97f6f469425972296cc8d29545d409eaa
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-03 13:04:17 +0200

    feat: add func for air pollution data

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,9 @@
-import { Weather } from "./types";
+import { AirPollution, ForecastWeather, CurrentWeather } from "./types";
 
 export const GEO = "http://api.openweathermap.org/geo/1.0";
 export const OPEN_WEATHER_API = "https://api.openweathermap.org/data/2.5/";
 export const OPEN_WEATHER_API_KEY = "0e5f3fe5505c6d06452416fd5256f98d";
+export const DEFAULT_CITY = "Warszawa";
 
 async function callApi(url: string) {
   const response = await fetch(url);
@@ -11,27 +12,13 @@ async function callApi(url: string) {
 }
 
 export async function getCurrentWeather(
-  lat: number,
-  lon: number,
+  city: string,
   units: string
-): Promise<Weather> {
+): Promise<CurrentWeather> {
+  const [lat, lon] = await getCords(city);
   const url = `${OPEN_WEATHER_API}/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=${units}&lang=pl`;
-  console.log(url);
   const response = await callApi(url);
-
-  return {
-    city: response["name"],
-    wind_speed: response["wind"]["speed"],
-    pressure: response["main"]["pressure"],
-    visibility: response["visibility"],
-    air_quality: 1,
-    temp: response["main"]["temp"],
-    humidity: response["main"]["humidity"],
-    description: response["weather"]["0"]["description"],
-    sunset: response["sys"]["sunset"],
-    sunrise: response["sys"]["sunrise"],
-    rain: response["rain"] ? response["rain"]["1h"] : 0,
-  };
+  return response as CurrentWeather;
 }
 
 export async function getCords(city: string) {
@@ -40,19 +27,16 @@ export async function getCords(city: string) {
   return [cords[0]["lat"], cords[0]["lon"]];
 }
 
-export async function getForecastWeather(
-  lat: number,
-  lon: number,
-  units: string
-) {
+export async function getForecastWeather(city: string, units: string) {
+  const [lat, lon] = await getCords(city);
   const url = `${OPEN_WEATHER_API}/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=${units}&lang=pl`;
-  console.log(url);
   const response = await callApi(url);
+  return response as ForecastWeather;
+}
 
-  const r = response.list.slice(0, 5).map((item) => {
-    const { temp_min, temp_max } = item["main"];
-    const description = item["weather"][0]["main"];
-    return { description, temp_min, temp_max };
-  });
-  return r;
+export async function getAirPollution(city: string, units: string) {
+  const [lat, lon] = await getCords(city);
+  const url = `${OPEN_WEATHER_API}/air_pollution?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=${units}&lang=pl`;
+  const response = await callApi(url);
+  return response as AirPollution;
 }
