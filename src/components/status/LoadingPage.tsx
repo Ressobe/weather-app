1c37f05941bb331013688d826ced8f11848556b3
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-03 15:08:48 +0200

    fix: fix background color

--- src/components/status/LoadingPage.tsx
+++ src/components/status/LoadingPage.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <main className="bg-[#d5d6d9] h-screen flex items-center justify-center">
+    <main className="bg-background h-screen flex items-center justify-center">
       <div className="lds-ring">
         <div></div>
         <div></div>
