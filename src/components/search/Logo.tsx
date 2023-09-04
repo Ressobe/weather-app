fe7124f0df13b1175befbbb8259d6290e550f8cf
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-04 13:10:34 +0200

    initial commit

---
+++ src/components/search/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className='text-primary font-bold text-4xl flex justify-center items-center'>
+      <img src='/icon.svg' className='inline-block mr-5' width='60px' />
+      <span className='text-on_surface'>WeatherXXX</span>
+    </div>
+  );
+}
