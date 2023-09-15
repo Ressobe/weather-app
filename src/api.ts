e7db72394dea26ebae97e5a23e58ed9c17c2fac8
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-15 12:43:15 +0200

    remove api key

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ import {
 
 export const GEO = "http://api.openweathermap.org/geo/1.0";
 export const OPEN_WEATHER_API = "https://api.openweathermap.org/data/2.5/";
-export const OPEN_WEATHER_API_KEY = "0e5f3fe5505c6d06452416fd5256f98d";
+export const OPEN_WEATHER_API_KEY = "";
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const DEFAULT_CITY = "Warszawa";
 
