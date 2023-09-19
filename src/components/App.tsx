5bb081e9c12d0768d3fb7f8d00fe39f14332e97b
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-19 21:02:00 +0200

    fix: remove setUnits because i will implement change units feature later

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -26,7 +26,7 @@ function checkCity() {
 
 function App() {
   const [city, setCity] = useState(checkCity());
-  const [units, setUnits] = useState("metric");
+  const units = "metric";
   const windowSize = useWindow();
 
   const weather = useQueries({
@@ -56,31 +56,31 @@ function App() {
   if (isError) return <ErrorPage />;
 
   return (
-    <main className=''>
+    <main className="">
       {windowSize.innerWidth > 1400 ? (
-        <div className='flex flex-wrap gap-5 justify-between items-center pt-6 px-4 mx-12'>
+        <div className="flex flex-wrap gap-5 justify-between items-center pt-6 px-4 mx-12">
           <CurrentLocation mobileView={false} setLocation={setCity} />
           <SearchBar defaultCity={city} mobileView={false} onSearch={setCity} />
           <Logo mobileView={false} />
         </div>
       ) : (
-        <div className='flex flex-wrap gap-10 justify-between items-center pt-6 px-4 mx-12'>
+        <div className="flex flex-wrap gap-10 justify-between items-center pt-6 px-4 mx-12">
           <SearchBar defaultCity={city} mobileView={true} onSearch={setCity} />
-          <div className='flex gap-10 items-center justify-center'>
+          <div className="flex gap-10 items-center justify-center">
             <CurrentLocation mobileView={true} setLocation={setCity} />
             <Logo mobileView={true} />
           </div>
         </div>
       )}
 
-      <section className='my-20 mx-10 gap-10 flex flex-col 2xl:flex-row justify-center'>
-        <div className='flex flex-col lg:flex-row justify-center'>
-          <div className="flex flex-wrap 2xl:flex-nowrap justify-center items-center gap-20  bg-on_background rounded-3xl p-20"> 
+      <section className="my-20 mx-10 gap-10 flex flex-col 2xl:flex-row justify-center">
+        <div className="flex flex-col lg:flex-row justify-center">
+          <div className="flex flex-wrap 2xl:flex-nowrap justify-center items-center gap-20  bg-on_background rounded-3xl p-20">
             <Weather currentWeather={weather[0].data} city={city} />
             <Forecast forecastWeather={weather[1].data} />
           </div>
         </div>
-        <div className='flex justify-center'>
+        <div className="flex justify-center">
           <TodayHighlights
             currentWeather={weather[0].data}
             airPollution={weather[2].data}
