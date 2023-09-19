ed5d551cd70b5df687566ee64f2e1f95d24320d5
Bartosz Sobina <bartosz.sobina@tutanota.com>
2023-09-19 21:29:10 +0200

    fix: use env for api key

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ import {
 
 export const GEO = "http://api.openweathermap.org/geo/1.0";
 export const OPEN_WEATHER_API = "https://api.openweathermap.org/data/2.5/";
-export const OPEN_WEATHER_API_KEY = "";
+export const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY;
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const DEFAULT_CITY = "Warszawa";
 
